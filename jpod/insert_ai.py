308047a5ab77af1a1d5351a0234ba4164f106c7d
MatthNig <matthias.niggli@unibas.ch>
2022-08-24 14:21:35 +0200

    stored postings with connection to AI in new table using cluster

--- jpod/insert_ai.py
+++ jpod/insert_ai.py
@@ -3,18 +3,19 @@ import navigate as nav
 import datagen as dg
 import pandas as pd
 
-# connect to JPOD
+#### connect to JPOD
 DB_DIR = sys.argv[1]
 JPOD_CONN = nav.db_connect(db_path = DB_DIR)
 
-# load AI keywords used by Acemoglu et al. (2019)
-df = pd.read_csv(DB_DIR + "acemoglu_ai_keywords.csv")
+#### load AI keywords and translations used by Acemoglu et al. (2019)
+df = pd.read_csv(DB_DIR + "augmentation_data/acemoglu_ai_keywords.csv")
 for v in ["en", "de", "fr", "it"]:
    df["keyword_" + v] = [w.replace(r"%", r"@%") for w in df["keyword_" + v]]
    df["keyword_" + v] = [w.replace(r"_", r"@_") for w in df["keyword_" + v]]
    df["keyword_" + v] = [w.replace(r"'", r"''") for w in df["keyword_" + v]]
 
-# define keywords and SQL query for the techfield:
+#### define keywords and SQL query for the techfield:
+keywords = []
 for v in ["en", "de", "fr", "it"]:
     keywords += list(df.loc[:, "keyword_" + v])
     keywords = list(set(keywords))
@@ -24,14 +25,14 @@ JPOD_QUERY = dg.keyword_query(
     output_variables = ["uniq_id"]
     )
     
-# retrieve and annotate postings with a connection to the techfield:
+#### retrieve and annotate postings with a connection to the techfield:
 res = pd.read_sql(con = JPOD_CONN, sql=JPOD_QUERY)
 if len(res) > 0:
-    print("Searching for job postings with connection to AI completed. Number of postings retrieved: {}".format(len(tmp)))
+    print("Searching for job postings with connection to AI completed. Number of postings retrieved: {}".format(len(res)))
 else:
     print("Searching for job postings with connection to AI completed. Number of postings retrieved: 0")
 
-# create new table "acemoglu_ai" and insert data into the table
+#### create new table "acemoglu_ai" and insert data into the table
 JPOD_QUERY = """
 DROP TABLE IF EXISTS acemoglu_ai;
 CREATE TABLE acemoglu_ai(
@@ -42,13 +43,6 @@ CREATE TABLE acemoglu_ai(
 JPOD_CONN.executescript(JPOD_QUERY)
 res.to_sql(name = "acemoglu_ai", con = JPOD_CONN, if_exists="append", index=False)
 
-# EXAMPLE: Number of postings with connection to technologies from Bloom et al. (2021):
-JPOD_QUERY = """
-SELECT pc.nuts_2, COUNT(DISTINCT(pc.uniq_id)) AS N_ai_postings
-FROM position_characteristics pc
-WHERE pc.uniq_id IN (SELECT uniq_id FROM acemoglu_ai)
-GROUP BY nuts_2
-ORDER BY N_ai_postings DESC;
-"""
-print("EXAMPLE: Number of postings with connection to technologies from Bloom et al. (2021):")
-print(pd.read_sql(con = JPOD_CONN, sql = JPOD_QUERY))
+#### Save and close connection to .db -------------------
+JPOD_CONN.commit()
+JPOD_CONN.close()
--- jpod_augment.sh
+++ jpod_augment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=add_emtech_jpod
+#SBATCH --job-name=add_ai_jpod
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH --qos=6hours
 
-#SBATCH --output=add_emtech_logs
-#SBATCH --error=add_emtech_errors
+#SBATCH --output=add_ai_logs
+#SBATCH --error=add_ai_errors
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=matthias.niggli@unibas.ch
 
@@ -15,4 +15,4 @@
 jpod_path="/scicore/home/weder/GROUP/Innovation/05_job_adds_data/"
 ml load Python/3.9.5-GCCcore-10.3.0
 source "${jpod_path}/jpod_venv/bin/activate"
-python "${jpod_path}jpod/jpod/insert_disruptech.py" $jpod_path
+python "${jpod_path}jpod/jpod/insert_ai.py" $jpod_path
