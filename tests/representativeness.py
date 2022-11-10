4f48fcd2cb3bdef1e8fb8cb2b3f2c7af58c3143e
matthnig <matthias.niggli@unibas.ch>
2022-11-10 17:06:09 +0100

    init tests for representativeness and uniqueness

--- tests/representativeness.py
+++ tests/representativeness.py
@@ -1,25 +1,49 @@
-#### Representatives ####
-# BFS employment data taken for 2Q22 from: 
-# https://www.pxweb.bfs.admin.ch/pxweb/de/px-x-0602000000_102/-/px-x-0602000000_102.px/
-# bfs <- read.csv(
-#   file = "/scicore/home/weder/GROUP/Innovation/05_job_adds_data/augmentation_data/ch_total_employed_people_02_2022.csv",
-#   sep = ";")
-# df <- read.csv("data/plot1_df.csv")
-# df <- df %>% 
-#   na.omit() %>%
-#   mutate(ch_total_share = total_postings / df[df$nuts_2 == "CH0", ]$total_postings[2]) %>%
-#   select(nuts_2, Grossregion, ch_total_share) %>%
-#   merge(bfs, by = "nuts_2") %>%
-#   select(nuts_2, Grossregion, employed_share, ch_total_share) %>%
-#   mutate(diff_share = ch_total_share / employed_share) %>%
-#   arrange(diff_share)
-# df
+from jpod import navigate as nav
+from jpod import datagen as dg
+import pandas as pd
+import sqlite3
 
-# nuts_2              Grossregion employed_share ch_total_share diff_share
-# 1   CH07                   ticino     0.04544429     0.01018492  0.2241187
-# 2   CH01         région lemanique     0.19204901     0.10411934  0.5421498
-# 3   CH05      eastern switzerland     0.12749293     0.11130664  0.8730417
-# 4   CH02        espace mittelland     0.20242806     0.17744788  0.8765972
-# 5   CH03 northwestern switzerland     0.13296988     0.13513996  1.0163200
-# 6   CH06      central switzerland     0.09873514     0.11616087  1.1764896
-# 7   CH04                   zurich     0.20088068     0.28011824  1.3944508
+# connect to the databse
+DB_DIR = "C:/Users/matth/Desktop/"
+DB_VERSION = "jpod_test.db"
+JPOD_CONN = sqlite3.connect(DB_DIR + DB_VERSION)
+
+#### Representativeness wrt spatial distribution of employemnt in CH -------------------------------------
+print("Performing tests for spatial distribution:")
+
+# get regional distribution of job postings in the database
+JPOD_QUERY = """
+SELECT COUNT(*) as total_postings, pc.nuts_2, rg.name_en AS Grossregion
+FROM position_characteristics pc
+LEFT JOIN (
+    SELECT nuts_2, name_en
+    FROM regio_grid
+    WHERE nuts_3 IS NULL AND nuts_2 IS NOT NULL
+    ) rg on pc.nuts_2 = rg.nuts_2
+GROUP BY pc.nuts_2
+"""
+postings_dist = pd.read_sql_query(JPOD_QUERY, con=JPOD_CONN)
+postings_dist["postings_share"] = postings_dist["total_postings"] / sum(postings_dist["total_postings"])
+postings_dist = postings_dist.dropna()
+
+# get the distribution of employment in Switzerland for 2Q2022 from BFS at
+# https://www.pxweb.bfs.admin.ch/pxweb/de/px-x-0602000000_102/-/px-x-0602000000_102.px/
+emp_dist = pd.read_csv("data/ch_total_employed_people_02_2022.csv", sep = ";")
+
+# compare the distributions:
+dist = postings_dist.merge(emp_dist, on = "nuts_2")
+dist["abs_diff"] = dist["postings_share"] - dist["employed_share"]
+dist["rel_diff"] = dist["postings_share"] / dist["employed_share"] - 1
+print(dist.loc[:, ["nuts_2", "Grossregion", "postings_share", "employed_share", "abs_diff", "rel_diff"]])
+#  nuts_2               Grossregion  postings_share  employed_share  abs_diff  rel_diff
+# 0   CH01          région lemanique        0.103264        0.192049 -0.088785 -0.462306
+# 1   CH02         espace mittelland        0.177068        0.202428 -0.025360 -0.125281
+# 2   CH03  northwestern switzerland        0.137581        0.132970  0.004611  0.034678
+# 3   CH04                    zurich        0.280082        0.200881  0.079202  0.394271
+# 4   CH05       eastern switzerland        0.109990        0.127493 -0.017503 -0.137285
+# 5   CH06       central switzerland        0.112544        0.098735  0.013808  0.139854
+# 6   CH07                    ticino        0.010899        0.045444 -0.034545 -0.760160
+
+# => Lake Geneva and Ticino are likely strongly underrepresented (thus alos bias possible)
+# => Zurich is likely strongly overrepresented
+# => Espace Mittelland & Eastern Switzerland somewhat underrepresented, Central Switzerland somewhat overrepresented
--- tests/uniqueness.py
+++ tests/uniqueness.py
@@ -1,19 +1,50 @@
-#### along the following lines:
-# # firm <- 'meag munich ergo assetmanagement gmbh'
-# # firm <- 'login berufsbildung ag'
-# # firm <- 'bouygues energies & services'
-# firm <- 'galliker transport ag'
-# # firm <- 'facebook'
-# JPOD_QUERY <- paste0("
-# SELECT jp.job_description
-# --SELECT COUNT(*) AS total_postings, COUNT(DISTINCT(jp.job_description)) AS n_unique
-# FROM job_postings jp
-# WHERE jp.uniq_id IN (
-#   SELECT uniq_id 
-#   FROM position_characteristics 
-#   WHERE company_name == '", firm, "'
-#   ) 
-# LIMIT 2
-# ")
-# jpodRetrieve(jpod_conn = JPOD_CONN, sql_statement = JPOD_QUERY)
+import pandas as pd
+import sqlite3
 
+# connect to the databse
+DB_DIR = "C:/Users/matth/Desktop/"
+DB_VERSION = "jpod_test.db"
+JPOD_CONN = sqlite3.connect(DB_DIR + DB_VERSION)
+
+# choose a random sample of 50 employers in the database
+JPOD_QUERY ="""
+SELECT company_name
+FROM institutions 
+WHERE company_name IN (SELECT company_name FROM institutions ORDER BY RANDOM() LIMIT %d);
+""" % 1000
+sample_employers = JPOD_CONN.execute(JPOD_QUERY).fetchall()
+sample_employers = [e[0] for e in sample_employers]
+
+# 1) total number of postings for each of these companies
+JPOD_QUERY ="""
+SELECT company_name, COUNT(uniq_id) AS total_postings
+FROM position_characteristics 
+WHERE company_name IN ({0})
+GROUP BY company_name
+ORDER BY -total_postings;
+""".format(str(sample_employers)[1:-1])
+n_postings = pd.read_sql(con = JPOD_CONN, sql = JPOD_QUERY)
+
+# 2) all postings for each of these companies
+JPOD_QUERY ="""
+SELECT jp.uniq_id, pc.company_name, jp.job_description
+FROM job_postings jp
+LEFT JOIN (
+    SELECT pc.company_name, pc.uniq_id
+    FROM position_characteristics pc
+    ) pc on pc.uniq_id = jp.uniq_id
+WHERE jp.uniq_id IN (
+    SELECT uniq_id 
+    FROM position_characteristics 
+    WHERE company_name IN ({0})
+    )
+""".format(str(sample_employers)[1:-1])
+all_postings = pd.read_sql(con = JPOD_CONN, sql = JPOD_QUERY)
+res = all_postings.merge(n_postings, on = "company_name")
+
+# 3) unique number of postings for each of these companies
+n_unique_postings = res.groupby(["company_name"])["job_description"].nunique().rename("unique_postings")
+res = res.merge(n_unique_postings, on = "company_name")
+res["share_unique"] = res["unique_postings"] / res["total_postings"]
+res = res.sort_values(["share_unique"]).drop_duplicates(["company_name"]).drop(["uniq_id", "job_description"], axis = 1)
+print(res.head(10))
