496006ce7cab9a383851e398d3d66af22136d2cc
matthnig <matthias.niggli@unibas.ch>
2022-11-11 11:35:38 +0100

    updated geo_grid table, disentangled creation of regio_grid table and regional assignment of positions.

---
+++ jpod/add_geogrid.py
@@ -0,0 +1,41 @@
+import sys
+from jpod import navigate as nav
+import pandas as pd
+import sqlite3
+
+#### connect to JPOD
+DB_DIR = sys.argv[1]
+DB_VERSION = sys.argv[2]
+JPOD_CONN = sqlite3.connect(DB_DIR + DB_VERSION)
+
+#### load data
+df = pd.read_csv("data/regio_grid.csv", sep = ";")
+df[["name_en", "name_de", "name_fr"]] = df[["name_en", "name_de", "name_fr"]].apply(lambda x: x.str.lower())
+
+#### create regio table in JPOD 
+JPOD_QUERY = """
+DROP TABLE IF EXISTS regio_grid;
+CREATE TABLE regio_grid (
+name_en TEXT NOT NULL,
+name_de TEXT,
+name_fr TEXT,
+regio_abbrev VARCHAR(5),
+nuts_level INT,
+oecd_level INT,
+iso_2 VARCHAR(2),
+iso_3 VARCHAR(3),
+nuts_0 VARCHAR(2),
+nuts_1 VARCHAR(3),
+nuts_2 VARCHAR(4),
+nuts_3 VARCHAR(5),
+oecd_tl1 VARCHAR(2),
+oecd_tl2 VARCHAR(4),
+oecd_tl3 VARCHAR(5),
+PRIMARY KEY (name_en, nuts_3)
+);
+"""
+JPOD_CONN.executescript(JPOD_QUERY)
+JPOD_CONN.commit()
+assert "regio_grid" in nav.get_tables(conn = JPOD_CONN)
+assert all([col in nav.get_table_vars(conn = JPOD_CONN, table = "regio_grid") for col in df.columns])
+df.to_sql("regio_grid", con = JPOD_CONN, if_exists="append", index=False)
--- jpod/assign_geo.py
+++ jpod/assign_geo.py
@@ -9,34 +9,9 @@ assert float(sqlite3.sqlite_version[:4]) >= 3.33, "SQLITE version must be 3.33.0
 DB_DIR = sys.argv[1]
 DB_VERSION = sys.argv[2]
 JPOD_CONN = sqlite3.connect(DB_DIR + DB_VERSION)
-df = pd.read_csv(DB_DIR + "augmentation_data/regio_grid.csv", sep = ";")
-df[["name_en", "name_de", "name_fr"]] = df[["name_en", "name_de", "name_fr"]].apply(lambda x: x.str.lower())
-
-#### create regio table in JPOD --------------------------------------
-JPOD_QUERY = """
-DROP TABLE IF EXISTS regio_grid;
-CREATE TABLE regio_grid (
-name_en TEXT NOT NULL,
-name_de TEXT,
-name_fr TEXT,
-regio_abbrev VARCHAR(5),
-nuts_0 VARCHAR(2),
-nuts_1 VARCHAR(3),
-nuts_2 VARCHAR(4),
-nuts_3 VARCHAR(5),
-oecd_tl1 VARCHAR(2),
-oecd_tl2 VARCHAR(4),
-oecd_tl3 VARCHAR(5),
-PRIMARY KEY (name_en, nuts_3)
-);
-"""
-JPOD_CONN.executescript(JPOD_QUERY)
-JPOD_CONN.commit()
-assert "regio_grid" in nav.get_tables(conn = JPOD_CONN)
-assert all([col in nav.get_table_vars(conn = JPOD_CONN, table = "regio_grid") for col in df.columns])
-df.to_sql("regio_grid", con = JPOD_CONN, if_exists="append", index=False)
 
 #### match postings to regions --------------------------------------
+assert "regio_grid" in nav.get_tables(conn = JPOD_CONN) # check if "regio_grid" table exists
 for geo_level in ["nuts_2", "nuts_3"]:
     if geo_level in nav.get_table_vars(conn = JPOD_CONN, table = "position_characteristics"):
         JPOD_CONN.execute("UPDATE position_characteristics  SET %s = NULL" % (geo_level))
@@ -46,7 +21,6 @@ for geo_level in ["nuts_2", "nuts_3"]:
         JPOD_QUERY = dg.geo_query(insert_table="position_characteristics", insert_variable= geo_level, matching_variable=match_var)
         JPOD_CONN.executescript(JPOD_QUERY)
         print("Terminated insertion of geographical level '{0}' based on string-matching in column '{1}'.".format(geo_level, match_var))
-
 
 #### custom matching procedure to grab more rows for the biggest non-matched entities --------------------------------------
 JPOD_QUERY = """
