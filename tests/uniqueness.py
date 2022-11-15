f48817e16c4cdf8230892ca296aaa0cebdb9051f
matthnig <matthias.niggli@unibas.ch>
2022-11-15 17:11:08 +0100

    init functionality to detect duplicate postings labelled under different employers

--- tests/uniqueness.py
+++ tests/uniqueness.py
@@ -6,9 +6,102 @@ import sqlite3
 DB_DIR = sys.argv[1]
 DB_VERSION = sys.argv[2]
 JPOD_CONN = sqlite3.connect(DB_DIR + DB_VERSION)
+JPOD_CONN = sqlite3.connect("C:/Users/nigmat01/Desktop/jpod_test.db")
+
+#################### BETWEEN EMPLOYERS #########################
+# choose a random sample of N employers in the database
+N = 100
+JPOD_QUERY ="""
+SELECT company_name
+FROM institutions 
+WHERE company_name IN (SELECT company_name FROM institutions ORDER BY RANDOM() LIMIT %d);
+""" % N
+sample_employers = JPOD_CONN.execute(JPOD_QUERY).fetchall()
+sample_employers = [e[0] for e in sample_employers]
+print("Retrieved number of sample employers: ", len(sample_employers))
+
+# choose a random sample of (N * 1000) postings from other employers
+JPOD_QUERY ="""
+SELECT uniq_id
+FROM job_postings
+WHERE uniq_id IN (
+    SELECT uniq_id 
+    FROM position_characteristics
+    WHERE company_name NOT IN ({0})
+    )
+ORDER BY RANDOM()
+LIMIT {1}
+""".format(str(sample_employers)[1:-1], N * 1000)
+benchmark_postings = JPOD_CONN.execute(JPOD_QUERY).fetchall()
+benchmark_postings = [p[0] for p in benchmark_postings]
+# JPOD_QUERY="""
+# SELECT DISTINCT uniq_id, job_description
+# FROM job_postings
+# WHERE uniq_id IN ({0})
+# """.format(str(benchmark_postings)[1:-1])
+JPOD_QUERY="""
+SELECT uniq_id, rn
+FROM (
+    SELECT uniq_id, ROW_NUMBER() OVER(PARTITION BY job_description ORDER BY uniq_id DESC) rn
+    FROM job_postings
+    WHERE uniq_id IN ({0})
+    )
+WHERE rn = 1
+""".format(str(benchmark_postings)[1:-1])
+benchmark_postings = JPOD_CONN.execute(JPOD_QUERY).fetchall()
+benchmark_postings = [p[0] for p in benchmark_postings]
+print("Retrieved number of unique benchmark postings: ", len(benchmark_postings))
+
+# check for each employer if its postings have counterparts in those of benchmark postings
+df = pd.DataFrame()
+for e in sample_employers[:10]:
+    res = {}
+    res["employer"] = e
+    res["n_postings"] = JPOD_CONN.execute("SELECT COUNT(*) FROM position_characteristics WHERE company_name = '%s'" % e.replace(r"'", r"''")).fetchone()[0]
+
+    JPOD_QUERY ="""
+    SELECT job_description, uniq_id
+    FROM job_postings 
+    WHERE uniq_id IN (SELECT uniq_id FROM position_characteristics WHERE company_name = '%s')
+    """ % e.replace(r"'", r"''")
+    employer_postings = JPOD_CONN.execute(JPOD_QUERY).fetchall()
+    employer_postings = [p[0] for p in employer_postings]
+    res["n_distinct_postings"] = len(set(employer_postings))
+    
+    # n_counterparts = 0
+    # for p in employer_postings:
+    #     JPOD_QUERY ="""
+    #     SELECT COUNT(*)
+    #     FROM job_postings
+    #     WHERE uniq_id IN ({0}) AND job_description == '{1}'
+    #     """.format(str(benchmark_postings)[1:-1], p.replace(r"'", r"''"))
+    #     n_counterparts =+ JPOD_CONN.execute(JPOD_QUERY).fetchone()[0]
+    JPOD_QUERY ="""
+    SELECT DISTINCT job_description
+    --SELECT COUNT(DISTINCT job_description)
+    FROM (
+        SELECT job_description
+        FROM job_postings
+        WHERE uniq_id IN (SELECT uniq_id FROM position_characteristics WHERE company_name == '{0}')
+        OR uniq_id IN ({1})
+        )
+    """.format(e.replace(r"'", r"''"), str(benchmark_postings)[1:-1])
+    total_distinct = JPOD_CONN.execute(JPOD_QUERY).fetchall()
+    # total_distinct = JPOD_CONN.execute(JPOD_QUERY).fetchone()[0]
+    res["n_counterparts"] = len(benchmark_postings) + res["n_distinct_postings"] - len(total_distinct)
+
+    res = pd.DataFrame(res, columns=res.keys(), index=range(1))
+    df = pd.concat([df, res], axis = 0)
+
+df = df.reset_index().drop("index", axis = 1).sort_values("n_counterparts", ascending= False)
+print(df.head(10))
+sum(df["n_counterparts"])
+
+# COUNT(DISTINCT job_description) returns not all values...
+
+##################### AMONG EMPLOYERS ###########################
 
 print("Performing tests for uniqueness of job postings:")
-
 # choose a random sample of N employers in the database
 N = 1000
 JPOD_QUERY ="""
