efe0eae100b2d06bf21eb79a7f2297af9d26ca7b
MatthNig <matthias.niggli@unibas.ch>
2022-11-15 10:50:44 +0100

    performed tests for representativeness of temporal subsample on jpod.db

--- scicore/jpod_augment.sh
+++ scicore/jpod_augment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=add_ai_jpod
+#SBATCH --job-name=jpod_tests
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 
 #SBATCH --time=01:00:00
 #SBATCH --qos=6hours
 
-#SBATCH --output=add_ai_logs
-#SBATCH --error=add_ai_errors
+#SBATCH --output=tests/jpod_tests_logs
+#SBATCH --error=tests/jpod_tests_logs
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=matthias.niggli@unibas.ch
 
@@ -16,4 +16,4 @@ jpod_path="/scicore/home/weder/GROUP/Innovation/05_job_adds_data/"
 jpod_version="jpod.db"
 ml load Python/3.9.5-GCCcore-10.3.0
 source "${jpod_path}/jpod_venv/bin/activate"
-python "${jpod_path}jpod/jpod/detect_ai.py" $jpod_path $jpod_version
+python "${jpod_path}jpod/tests/date_representativeness.py" $jpod_path $jpod_version
--- tests/date_representativeness.py
+++ tests/date_representativeness.py
@@ -2,7 +2,7 @@ import sqlite3
 import sys
 import pandas as pd
 from matplotlib import pyplot as plt
-from tests import jpod_tests
+import jpod_tests
 
 # connect to the databse --------------------------------
 DB_DIR = sys.argv[1]
@@ -16,10 +16,15 @@ for m in months:
     geo_dist = pd.concat([geo_dist, jpod_tests.get_geo_dist(con =JPOD_CONN, month = m, nuts_level=2)])
 print("Number of postings per time window:")
 print(geo_dist.groupby(["sample"])["total_postings"].sum())
+#Number of postings per time window:
+#sample
+#2021-01         153646
+#2021-05         175460
+#2021-09         165916
+#full_sample    3211219
 plot_df = geo_dist.dropna()
 jpod_tests.plot_dist(df = plot_df)
 plt.legend()
-#plt.show()
 plt.savefig("tests/img/nuts_2_dist.png")
 
 # B) get nuts_3 distribution of full sample & snapshot sample --------------------------------
@@ -41,6 +46,12 @@ for m in months:
     tech_shares = pd.concat([tech_shares, jpod_tests.get_tech_share(con = JPOD_CONN, month = m, tech = tech)])
 print("Distribution of job postings with connection to Bloom (2021) disruptive technologies:")
 print(tech_shares)
+#Distribution of job postings with connection to Bloom (2021) disruptive technologies:
+#   tech_postings       sample        tech  total_postings     share
+#0         201543  full_sample  bloom_tech         3211219  0.062762
+#0          10457      2021-05  bloom_tech          175460  0.059598
+#0           8070      2021-09  bloom_tech          165916  0.048639
+#0           8578      2021-01  bloom_tech          153646  0.055830
 
 # D) get bloom field distribution of full sample & snapshot sample --------------------------------
 months = ["2021-05", "2021-09", "2021-01"]
@@ -73,6 +84,12 @@ for m in months:
     tech_shares = pd.concat([tech_shares, jpod_tests.get_tech_share(con = JPOD_CONN, month = m, tech = tech)])
 print("Distribution of job postings with connection to Acemoglu (2022) AI technology:")
 print(tech_shares)
+#Distribution of job postings with connection to Acemoglu (2022) AI technology:
+#   tech_postings       sample         tech  total_postings     share
+#0          33847  full_sample  acemoglu_ai         3211219  0.010540
+#0           1460      2021-05  acemoglu_ai          175460  0.008321
+#0           1099      2021-09  acemoglu_ai          165916  0.006624
+#0           1186      2021-01  acemoglu_ai          153646  0.007719
 
 # G) get spatial distribution of AI in full sample & snapshot sample--------------------------------
 tech = "ai"
