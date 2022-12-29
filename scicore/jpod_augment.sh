f9f2a430d1a00093391533e03efb0de3b62c3b2e
MatthNig <matthias.niggli@unibas.ch>
2022-12-29 12:32:54 +0100

    duplicates identified in jpod.db

--- scicore/jpod_augment.sh
+++ scicore/jpod_augment.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=duplicate_cleaning_test
+#SBATCH --job-name=duplicate_cleaning
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 
-#SBATCH --time=00:30:00
-#SBATCH --qos=30min
+#SBATCH --time=02:00:00
+#SBATCH --qos=6hours
 
-#SBATCH --output=tests/logs/duplicate_cleaning_time
-#SBATCH --error=tests/logs/duplicate_cleaning_time
+#SBATCH --output=tests/logs/duplicate_cleaning
+#SBATCH --error=tests/logs/duplicate_cleaning
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=matthias.niggli@unibas.ch
 
 ## add information to JPOD using Python
 jpod_path="/scicore/home/weder/GROUP/Innovation/05_job_adds_data/"
-jpod_version="jpod_test.db"
+jpod_version="jpod.db"
 ml load Python/3.9.5-GCCcore-10.3.0
 source "${jpod_path}/jpod_venv/bin/activate"
 python "${jpod_path}jpod/jpod/clean_duplicates.py" $jpod_path $jpod_version
