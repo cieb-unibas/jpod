95db6be65a044d070a38d0321536373e4eca9d16
MatthNig <matthias.niggli@unibas.ch>
2023-01-04 10:40:52 +0100

    rm log files re duplicate cleaning time and tests

--- scicore/jpod_augment.sh
+++ scicore/jpod_augment.sh
@@ -6,14 +6,14 @@
 #SBATCH --time=02:00:00
 #SBATCH --qos=6hours
 
-#SBATCH --output=tests/logs/duplicate_cleaning_new
-#SBATCH --error=tests/logs/duplicate_cleaning_new
+#SBATCH --output=tests/logs/duplicate_cleaning
+#SBATCH --error=tests/logs/duplicate_cleaning_errors
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=matthias.niggli@unibas.ch
 
 ## add information to JPOD using Python
 jpod_path="/scicore/home/weder/GROUP/Innovation/05_job_adds_data/"
-jpod_version="jpod_new.db"
+jpod_version="jpod.db"
 ml load Python/3.9.5-GCCcore-10.3.0
 source "${jpod_path}/jpod_venv/bin/activate"
 python "${jpod_path}jpod/jpod/clean_duplicates.py" $jpod_path $jpod_version
