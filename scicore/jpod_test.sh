1d485fe4b2315e9af94889dc298d367775ba1b52
MatthNig <matthias.niggli@unibas.ch>
2023-01-21 14:20:16 +0100

    verification tests for duplicate cleaning.

--- scicore/jpod_test.sh
+++ scicore/jpod_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=none_val
+#SBATCH --job-name=jpod_test_clean_duplicates
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 
-#SBATCH --time=00:20:00
-#SBATCH --qos=30min
+#SBATCH --time=02:00:00
+#SBATCH --qos=6hours
 
-#SBATCH --output=tests/logs/none_val
-#SBATCH --error=tests/logs/none_val
+#SBATCH --output=tests/logs/after_duplcleaning_tests
+#SBATCH --error=tests/logs/after_duplcleaning_tests_errors
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=matthias.niggli@unibas.ch
 
@@ -16,4 +16,4 @@ jpod_path="/scicore/home/weder/GROUP/Innovation/05_job_adds_data/"
 jpod_version="jpod.db"
 ml load Python/3.9.5-GCCcore-10.3.0
 source "${jpod_path}/jpod_venv/bin/activate"
-python "${jpod_path}jpod/tests/none_val.py" $jpod_path $jpod_version
+python "${jpod_path}jpod/tests/token_based_duplicates.py" $jpod_path $jpod_version
