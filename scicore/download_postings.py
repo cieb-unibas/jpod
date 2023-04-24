fbf6da61616b32f416290246d697ad3ebc009c9d
MatthNig <matthias.niggli@unibas.ch>
2023-04-24 14:47:44 +0200

    all ~6.3Mio postings from jobspickr stored on disk on sciocre

--- scicore/download_postings.py
+++ scicore/download_postings.py
@@ -11,12 +11,13 @@ def save_jobspickr(url, save_at):
     response = urlreq.urlopen(request)    
     with open(save_at, 'wb') as f:
         f.write(response.read())
-    print("file saved as %s" % destination)
+    print("file saved as %s" % save_at)
 
-if __name__ == "main":
+if __name__ == "__main__":
+    print("----------------Ready to download postings from jobspickr----------------")
     destination = "/scicore/home/weder/GROUP/Innovation/05_job_adds_data/jobspickr_raw/jobspickr2023/"
-    urls = get_config(config_file = "../jobspickr_config.txt")#"/scicore/home/weder/GROUP/Innovation/05_job_adds_data/jobspickr_raw/2023/jobspickr_config.csv")
+    urls = get_config(config_file = os.path.join(destination, "jobspickr_config.txt"))
     for u in urls:
         file_name = u.split("/")[-1]
         save_jobspickr(url = u, save_at = os.path.join(destination, file_name))
-    print("All files from JobsPickr saved to disk.")
+    print("----------------All files from JobsPickr saved to disk.----------------")
