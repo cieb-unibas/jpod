1e3bb8899dfc38ea0d14be855a1e1ecf8d9602e0
matthnig <matthias.niggli@unibas.ch>
2023-06-06 11:03:16 +0200

    implemented duplicate cleaning for updated data

--- jpod/clean_duplicates.py
+++ jpod/clean_duplicates.py
@@ -1,54 +1,57 @@
-import sys
+import os
 import sqlite3
 
 try:
     from jpod.datagen import DuplicateCleaner
+    import jpod.config as config
+    import jpod.navigate as nav
 except:
     from datagen import DuplicateCleaner
+    import config
+    import navigate as nav
 
-#### connect to JPOD: ------------------------------
-DB_DIR = sys.argv[1] # path to the .db
-DB_VERSION = sys.argv[2] # .db version
-JPOD_CONN = sqlite3.connect(DB_DIR + DB_VERSION)
-# JPOD_CONN = sqlite3.connect("C:/Users/nigmat01/Desktop/jpod_test.db")
-# JPOD_CONN = sqlite3.connect("C:/Users/matth/Desktop/jpod_test.db")
 
-#### prepare the .db: ------------------------------
-new_cols = [
-    "unique_posting_text", # exactly identical text
-    "unique_posting_textlocation", # identical text AND identical city
-    ]
+if __name__ == "__main__":
 
-existing_cols = [x[1] for x in JPOD_CONN.execute("PRAGMA table_info(job_postings)").fetchall()]
+    # parameters for connecting to JPOD
+    JPOD_VERSION = "jpod_test.db"
+    DATA_BATCH = "jobspickr_2023_01"
+    DB_DIR = os.path.join(nav.get_path(config.DB_DIRS), JPOD_VERSION)
+    JPOD_CONN = sqlite3.connect(database = DB_DIR)
+    print("---------------Starting to identify duplicated postings---------------")
+    
+    # check if the columns already exist and set them to their default values:
+    new_cols = [
+        "unique_posting_text", # no exactly identical text
+        "unique_posting_textlocation", # no identical text within city
+        ]
+    existing_cols = nav.get_table_vars(conn = JPOD_CONN, table = "job_postings")
+    for c in new_cols:
+        if c in existing_cols:
+            JPOD_CONN.execute("UPDATE job_postings SET %s = 'yes' WHERE data_batch == '%s'" % (c, DATA_BATCH))
+            print("Set values in column '%s' for data batch '%s' to default value 'yes'." % (c, DATA_BATCH))
+            JPOD_CONN.commit()
+        else:
+            JPOD_CONN.execute("ALTER TABLE job_postings ADD COLUMN %s VARCHAR(3) DEFAULT 'yes'" % c)
+            print("Initialized column %s" % c)
+            JPOD_CONN.commit()
 
-for c in new_cols:
-    if c in existing_cols:
-        JPOD_CONN.execute("UPDATE job_postings SET %s = 'yes'" % c)
-        print("Set column %s to defailt value 'yes'." % c)
-        JPOD_CONN.commit()
-    else:
-        JPOD_CONN.execute("ALTER TABLE job_postings ADD COLUMN %s VARCHAR(3) DEFAULT 'yes'" % c)
-        print("Initialized column %s" % c)
-        JPOD_CONN.commit()
+    # Identify duplicates:
+    cleaner = DuplicateCleaner(con = JPOD_CONN, data_batch = 'jobspickr_2023_01')
+    # a) exactly identical text
+    JPOD_QUERY = cleaner.duplicate_query(assign_to = "unique_posting_text", levels=["job_description"])
+    cleaner.find_duplicates(query = JPOD_QUERY)
+    # b) exactly identical text and city location
+    JPOD_QUERY = cleaner.duplicate_query(assign_to = "unique_posting_textlocation", levels=["job_description", "city"])
+    cleaner.find_duplicates(query = JPOD_QUERY)
 
-#### Identify duplicates: ------------------------------
-cleaner = DuplicateCleaner(con = JPOD_CONN, data_batch = 'jobspickr_2022_01')
+    # test and summarize:
+    for c in new_cols:
+        assert c in [x[1] for x in JPOD_CONN.execute("PRAGMA table_info(job_postings)").fetchall()], "Failed to create column '%s' in Table job_postings" %c
+        n_duplicated = JPOD_CONN.execute("SELECT COUNT(*) FROM job_postings WHERE %s == 'no'" % c).fetchone()[0]
+        n_unique = JPOD_CONN.execute("SELECT COUNT(*) FROM job_postings WHERE %s == 'yes'" % c).fetchone()[0]
+        print("Found %d unique and %d duplicated postings in JPOD for column '%s'" % (n_unique, n_duplicated, c))
 
-# exactly identical text
-JPOD_QUERY = cleaner.duplicate_query(assign_to = "unique_posting_text", levels=["job_description"])
-cleaner.find_duplicates(query=JPOD_QUERY)
-
-# exactly identical text and city location
-JPOD_QUERY = cleaner.duplicate_query(assign_to = "unique_posting_textlocation", levels=["job_description", "city"])
-cleaner.find_duplicates(query=JPOD_QUERY)
-
-#### Test and summarize: -----------------------------
-for c in new_cols:
-    assert c in [x[1] for x in JPOD_CONN.execute("PRAGMA table_info(job_postings)").fetchall()], "Failed to create column '%s' in Table job_postings" %c
-    n_duplicated = JPOD_CONN.execute("SELECT COUNT(*) FROM job_postings WHERE %s == 'yes'" % c).fetchone()[0]
-    n_unique = JPOD_CONN.execute("SELECT COUNT(*) FROM job_postings WHERE %s == 'no'" % c).fetchone()[0]
-    print("Found %d unique and %d duplicated postings in JPOD for column '%s'" % (n_unique, n_duplicated, c))
-
-#### Save and close connection to .db -------------------
-JPOD_CONN.commit()
-JPOD_CONN.close()
+    #### Save and close connection to .db
+    JPOD_CONN.commit()
+    JPOD_CONN.close()
