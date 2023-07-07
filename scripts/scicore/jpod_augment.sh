5b10c712a62d5c31c4a4574ae041296b53c5ec34
MatthNig <matthias.niggli@unibas.ch>
2023-07-07 09:42:11 +0200

    init duplicate identification

--- scripts/scicore/jpod_augment.sh
+++ scripts/scicore/jpod_augment.sh
@@ -6,23 +6,16 @@
 #SBATCH --time=06:00:00
 #SBATCH --qos=6hours
 
-#SBATCH --output=cluster_logs/augment_disruptech
-#SBATCH --error=cluster_logs/augment_disruptech_errors
+#SBATCH --output=cluster_logs/clean_duplicates
+#SBATCH --error=cluster_logs/clean_duplicates_errors
 #SBATCH --mail-type=END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=matthias.niggli@unibas.ch
 
-## add information to JPOD using Python
+## configure
 jpod_path="/scicore/home/weder/GROUP/Innovation/05_job_adds_data/"
 jpod_version="jpod.db"
 cd $jpod_path/jpod/
 source ../jpod_venv/bin/activate
 
-## add geographical information, detect postings with a connection to AI, disruptive technologies from Bloom(2021) and clean duplicates.
-## full augmentation
-## for f in detect_ai.py detect_disruptech.py clean_duplicates.py
-## do
-##  python ./scripts/$f
-## done
-
-## specific augmentation
+## add infromation to JPOD
 python ./scripts/clean_duplicates.py
