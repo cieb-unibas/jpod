523942882de384a74521d5ad6a5554acf74eb20a
MatthNig <matthias.niggli@unibas.ch>
2023-07-31 10:53:55 +0200

    identified some cases where geo assignment needs additional cleaning

--- main.py
+++ main.py
@@ -3,49 +3,72 @@ import jpod
 
 # connect to the database
 JPOD_PATH = jpod.get_path(potential_paths=jpod.config.DB_DIRS)
-JPOD_CONN = jpod.connect_to_jpod(db_location=JPOD_PATH, version="full")
+JPOD_CONN = jpod.connect_to_jpod(db_location=JPOD_PATH, version="test")
 
-# Get all ai jobs per region for the 2023 batch:
-AI_QUERY = """
-SELECT pc.company_name AS employer, pc.inferred_country AS country, rg.name_en AS region, rg.oecd_tl2 AS region_code
+pd.DataFrame(JPOD_CONN.execute("SELECT name_en, nuts_2, nuts_3, oecd_tl2, oecd_tl3 FROM regio_grid WHERE iso_2 = 'CN'").fetchall())
+jpod.get_table_vars(conn=JPOD_CONN, table = "regio_grid")
+
+JPOD_CONN.execute("SELECT DISTINCT inferred_state, nuts_2 FROM position_characteristics WHERE inferred_country = 'austria'").fetchall()
+query="""
+SELECT inferred_country, inferred_state, pc.nuts_2--, COUNT(aa.uniq_id)
 FROM acemoglu_ai aa
 INNER JOIN (
-    SELECT uniq_id
+    SELECT * 
+    FROM
+    position_characteristics 
+    WHERE inferred_country = 'germany') pc ON aa.uniq_id = pc.uniq_id
+LEFT JOIN (SELECT uniq_id
     FROM job_postings
     WHERE data_batch = 'jobspickr_2023_01' AND unique_posting_text = 'yes'
     ) jp ON aa.uniq_id = jp.uniq_id
-LEFT JOIN (
-    SELECT uniq_id, inferred_country, company_name, nuts_2
-    FROM position_characteristics
-    ) pc ON aa.uniq_id = pc.uniq_id
 LEFT JOIN(
     SELECT nuts_2, name_en, oecd_tl2
     FROM regio_grid
     WHERE oecd_level = 2
-    ) rg ON rg.oecd_tl2 = pc.nuts_2;
+    ) rg ON rg.oecd_tl2 = pc.nuts_2
+--GROUP BY inferred_country, inferred_state, nuts_2
+"""
+pd.DataFrame(JPOD_CONN.execute(query).fetchall())
+
+
+# Get all ai jobs per region for the 2023 batch:
+AI_QUERY = """
+SELECT country, region, region_code, n_postings
+FROM (
+    SELECT pc.inferred_country AS country,
+        pc.nuts_2 AS region_code,
+        COUNT(aa.uniq_id) AS n_postings
+    FROM acemoglu_ai aa
+    INNER JOIN (
+        SELECT uniq_id, inferred_country, company_name, inferred_state, nuts_2
+        FROM position_characteristics
+        ) pc ON aa.uniq_id = pc.uniq_id
+    LEFT JOIN (
+        SELECT uniq_id
+        FROM job_postings
+        WHERE data_batch = 'jobspickr_2023_01' AND unique_posting_text = 'yes'
+        ) jp ON aa.uniq_id = jp.uniq_id
+    GROUP BY country, region_code
+)
+LEFT JOIN (
+    SELECT name_en as region, oecd_tl2
+    FROM regio_grid
+    ) rg ON rg.oecd_tl2 = region_code
 """
 ai_jobs = pd.read_sql(con = JPOD_CONN, sql = AI_QUERY)
+ai_jobs
+
+JPOD_CONN.execute("UPDATE position_characteristics SET nuts_2 = 'AT13' WHERE inferred_state = 'wien';")
+
+# TO DO: 
+# => change nuts_2 value of Berlin to 'DE3' AND ile-de-france to FR1 and wien to 'AT13'
+# => change all the pc.nuts_2 to oecd_tl2 und oecd_tl3
+# => make this as a left-join pc & rg where oecd_level = 2 ON PC.inferred_state = rg.region_name
+# => set hong kong to 'CNHK'
 
 # Get overall number of jobs per region for the 2023 batch:
-TOTAL_QUERY = """
-SELECT pc.inferred_country AS country, rg.name_en AS region, pc.nuts_2 AS region_code, COUNT(pc.uniq_id) AS n_postings
-FROM (
-    SELECT uniq_id
-    FROM job_postings
-    WHERE data_batch = 'jobspickr_2023_01' AND unique_posting_text = 'yes'
-    ) jp
-LEFT JOIN (
-    SELECT uniq_id, inferred_country, nuts_2
-    FROM position_characteristics
-    ) pc ON jp.uniq_id = pc.uniq_id
-LEFT JOIN(
-    SELECT name_en, oecd_tl2
-    FROM regio_grid
-    WHERE oecd_level = 2
-    ) rg ON rg.oecd_tl2 = pc.nuts_2
-GROUP BY pc.nuts_2
-"""
-total_jobs = pd.read_sql(con = JPOD_CONN, sql = TOTAL_QUERY)
+# ...
+
 
 # combine a calculate shares
 df = ai_jobs["region"].value_counts().reset_index().rename(columns={"index": "region", "region": "ai_postings"})
